69a3a9cf087c6faa0f14d235500ff0bb703905b3
Emre Kumaş <emre-kumas@outlook.com>
2020-01-03 15:01:08 +0300

    Genetic Algorithm: File reading and creating adjacency matrix completed

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -3,6 +3,61 @@ import os.path
 import re
 
 
+# GLOBAL VARIABLES
+vertex_count = None
+edge_count = None
+vertex_weights = []
+adjacency_matrix = [[]]
+
+
+# noinspection PyUnusedLocal
+def read_file(file_name):
+
+    # Global variables...
+    global vertex_count, edge_count, vertex_weights, adjacency_matrix
+    file = open(file_name, 'r')
+
+    # Read the file till the end...
+    while True:
+        # Read the line and remove the newline character at the end
+        line = file.readline().rstrip('\n\r')
+        if not line:
+            # Means we reached to the end of file...
+            break
+
+        if vertex_count is None:
+            # Then this is the line for indicating the number of vertices...
+            vertex_count = int(line)
+
+            # After we get the vertex count, we initialize adjacency matrix with zeros...
+            adjacency_matrix = [[0 for i in range(vertex_count)] for i in range(vertex_count)]
+
+        elif edge_count is None:
+            # Then this is the line for indicating the number of edges...
+            edge_count = int(float(line))
+        elif len(vertex_weights) == 0:
+            # These are the lines indicating vertex weights...
+            for i in range(vertex_count):
+                # Get only the second part of the string...
+                _, _, line = line.partition(' ')
+                # Convert comma separated values into dot separated...
+                line = line.replace(',', '.')
+                # Add the value into vertex weights list as float type...
+                vertex_weights.append(float(line))
+
+                # We will read the next line, if this is not the last vertex's weight...
+                if i + 1 != vertex_count:
+                    line = file.readline().rstrip('\n\r')
+        else:
+            # These are the lines indicating edges between vertices...
+            line = line.split()
+            # The first element of line is row and second element of line is column...
+            adjacency_matrix[int(line[0])][int(line[1])] = 1
+
+    # Close the file...
+    file.close()
+
+
 def check_inputs():
     file_name = sys.argv[1]
 
@@ -39,4 +94,8 @@ if __name__ == '__main__':
         print("Usage: python genetic_algorithm.py graph_file num_generations pop_size crossover_prob mutation_prob")
         exit(-1)
 
+    # Validating inputs...
     file_name, num_generations, pop_size, crossover_prob, mutation_prob = check_inputs()
+
+    # Read graph file and create adjacency matrix...
+    read_file(file_name)
